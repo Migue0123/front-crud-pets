db6bc59d760955d8c5af8d4578835cbd9e78c268
Migue <miguel@MiguelHiguita.local>
2023-06-01 17:26:27 -0500

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+//Components
+import { PetListComponent } from './components/pet-list/pet-list.component';
+import { AddEditPetComponent } from './components/add-edit-pet/add-edit-pet.component';
+import { SeePetComponent } from './components/see-pet/see-pet.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'petList',
+    pathMatch: 'full',
+  },
+  {
+    path: 'petList',
+    component: PetListComponent,
+  },
+  {
+    path: 'addPet',
+    component: AddEditPetComponent,
+  },
+  {
+    path: 'seePet/:id',
+    component: SeePetComponent,
+  },
+  {
+    path: 'editPet/:id',
+    component: AddEditPetComponent,
+  },
+  {
+    path: '**',
+    redirectTo: 'petList',
+    pathMatch: 'full',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
